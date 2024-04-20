1154c3525810c45461f2c3666d44cbe354649e0a
Jaggy <jaggy@hey.com>
2024-04-20 16:00:57 +0800

    Replicate delete error

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return hybridly('welcome');
 });
+
+Route::post('/endpoint', function () {
+    dd('post');
+});
+
+Route::put('/endpoint', function () {
+    dd('put');
+});
+
+Route::delete('/endpoint', function () {
+    dd('delete');
+});
