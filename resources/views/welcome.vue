d05d58fda7ea1732ee0a97d41a6aeb57f9d3b85f
Jaggy <jaggy@hey.com>
2024-04-20 16:39:44 +0800

    Narrow down the bug

--- resources/views/welcome.vue
+++ resources/views/welcome.vue
@@ -9,16 +9,44 @@ const post = useForm({
     fields: {},
 })
 
-const put = useForm({
+const put_with_fields = useForm({
     method: 'PUT',
+    url: '/endpoint',
+    fields: { },
+})
+
+const put_without_fields = useForm({
+    method: 'PUT',
+    url: '/endpoint',
+})
+
+
+const put_with_method = useForm({
+    method: 'PUT',
+    url: '/endpoint',
+    fields: {
+        _method: 'PUT',
+    }
+})
+
+const destroy_with_fields = useForm({
+    method: 'DELETE',
     url: '/endpoint',
     fields: {},
 })
 
-const destroy = useForm({
+const destroy_without_fields = useForm({
     method: 'DELETE',
     url: '/endpoint',
-    fields: {},
+})
+
+
+const destroy_with_method = useForm({
+    method: 'DELETE',
+    url: '/endpoint',
+    fields: {
+        _method: 'DELETE',
+    }
 })
 
 const handlePost = () => router.post('/endpoint')
@@ -27,33 +55,79 @@ const handleDelete = () => router.delete('/endpoint')
 </script>
 
 <template layout>
-    <div>
+    <div class="grid">
         <div>
             <h2>useForm</h2>
 
-            <form @submit.prevent="post.submit">
-                <button>POST</button>
-            </form>
+            <div class="buttons">
+                <form @submit.prevent="post.submit">
+                    <button>POST</button>
+                </form>
 
-            <form @submit.prevent="put.submit">
-                <button>PUT</button>
-            </form>
+                <form @submit.prevent="put_with_fields.submit">
+                    <button>PUT WITH fields</button>
+                </form>
 
-            <form @submit.prevent="destroy.submit">
-                <button>DELETE</button>
-            </form>
+                <form @submit.prevent="put_without_fields.submit">
+                    <button>PUT WITHOUT fields</button>
+                </form>
+
+                <form @submit.prevent="put_with_method.submit">
+                    <button>PUT WITH _method</button>
+                </form>
+
+                <form @submit.prevent="destroy_with_fields.submit">
+                    <button>DELETE WITH fields</button>
+                </form>
+
+                <form @submit.prevent="destroy_without_fields.submit">
+                    <button>DELETE WITHOUT fields</button>
+                </form>
+
+                <form @submit.prevent="destroy_with_method.submit">
+                    <button>DELETE WITH _method</button>
+                </form>
+            </div>
         </div>
 
         <div>
             <h2>router</h2>
 
-            <button @click.prevent="handlePost">POST</button>
+            <div class="buttons">
+                <div>
+                    <button @click.prevent="handlePost">POST</button>
+                </div>
 
-            <button @click.prevent="handlePut">PUT</button>
+                <div>
+                    <button @click.prevent="handlePut">PUT</button>
+                </div>
 
-            <button @click.prevent="handleDelete">DELETE</button>
+                <div>
+                    <button @click.prevent="handleDelete">DELETE</button>
+                </div>
+            </div>
         </div>
     </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.grid {
+    padding: 12rem;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+}
+
+h2 {
+    font-size: 1.5rem;
+    font-weight: 700;
+    letter-spacing: -0.02em;
+    font-family: Monaco, monospace;
+}
+
+.buttons {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    margin-top: 2rem;
+}
+</style>
